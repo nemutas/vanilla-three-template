5a3dca976d016e43946f681ee4049ab098a1fec6
nemutas <inohiro.focru@gmail.com>
2022-07-15 00:57:34 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
-export default defineConfig({
-	root: './src',
-	base: '/vanilla-three-template/',
-	plugins: [glsl()],
-	build: {
-		rollupOptions: {
-			input: {
-				home: path.resolve(__dirname, './src/index.html')
-			}
-		},
-		outDir: '../dist'
+export default defineConfig(({ mode }) => {
+	console.log(mode)
+	return {
+		root: './src',
+		base: mode === 'development' ? '/' : '/vanilla-three-template/',
+		plugins: [glsl()],
+		build: {
+			rollupOptions: {
+				input: {
+					home: path.resolve(__dirname, './src/index.html')
+				}
+			},
+			outDir: '../dist'
+		}
 	}
 })
